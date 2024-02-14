f324325ba8fa419a720ff6464b7b8f921fa59f39
Anton Lazorenko <lazorenkoanton95@gmail.com>
2024-02-14 19:39:55 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+alert("Привет! Предлагаю тебе сыграть одну интересную игру! Правила предельно просты! Я загадаю число от 1 до 10, а ты должен его отгадать. У тебя будет одна попытка");
+let result = confirm('Ты готов приступить к игре?');
+if (result === false) {
+    alert("Возможно в другой раз. До свидания")
+}
+else {
+    const randomNumber = Math.floor(Math.random() * 10) + 1; 
+    let yourNumber = prompt("выбери число от 1 до 10")
+    let checkNumber = isNaN(yourNumber)
+    if ( checkNumber === true) {
+        alert("Выбрано не числовое значение")
+    }
+    else if (+yourNumber == randomNumber) {
+        alert('Поздравляю! Ты угадал') 
+    }
+    else if (yourNumber > 10) {
+        alert("Ты выбрал число больше чем 10")
+    } else if (yourNumber <= 0) {
+        alert("Ты выбрал число меньше чем 1")
+    } else {
+        alert("Неудача! Верное число:" +  " " + (randomNumber) + " " + "Попробуй снова!")
+    } 
+}
